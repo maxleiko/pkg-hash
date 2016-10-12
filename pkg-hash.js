c10dd556f6ed27ff86131c453b6dff3cff172ee8
Maxime Tricoire <max.tricoire@gmail.com>
2016-10-12 15:34:25 +0200

    v2.2.0: fix basepath context when resolving local files

--- pkg-hash.js
+++ pkg-hash.js
@@ -10,18 +10,19 @@ var LOCAL = /^(\.|\/)/;
 
 module.exports = function pkgHash(rootPath, done) {
   var pkg = require(path.join(rootPath, 'package.json'));
-  var entry = path.resolve(rootPath, pkg.main);
+  var entry = path.join(rootPath, pkg.main);
   fs.readFile(entry, 'utf8', function (err, src) {
     if (err) {
       done(err);
     } else {
+      var basepath = entry.substr(0, entry.length - path.basename(entry).length);
       var tasks = detective(src)
         .filter(function (moduleName) {
           return LOCAL.exec(moduleName);
         })
         .map(function (moduleName) {
           return function (cb) {
-            fs.readFile(require.resolve(path.join(rootPath, moduleName)), 'utf8', cb);
+            fs.readFile(require.resolve(path.join(basepath, moduleName)), 'utf8', cb);
           };
         });
 
