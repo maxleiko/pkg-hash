ff915921887f1666133e6e66c14e6db39d52fd9a
Maxime Tricoire <max.tricoire@gmail.com>
2016-12-22 12:58:35 +0100

    v3.0.0: going from async to sync + real require deep tree reading

--- pkg-hash.js
+++ pkg-hash.js
@@ -1,41 +1,54 @@
-'use strict';
-
-var crypto = require('crypto');
-var detective = require('detective');
-var async = require('async');
 var fs = require('fs');
 var path = require('path');
+var crypto = require('crypto');
+var detective = require('detective');
+var resolve = require('resolve');
 
 var LOCAL = /^(\.|\/)/;
 
-module.exports = function pkgHash(rootPath, done) {
-  var pkg = require(path.join(rootPath, 'package.json'));
-  var entry = path.join(rootPath, pkg.main);
-  fs.readFile(entry, 'utf8', function (err, src) {
-    if (err) {
-      done(err);
-    } else {
-      var basepath = entry.substr(0, entry.length - path.basename(entry).length);
-      var tasks = detective(src)
-        .filter(function (moduleName) {
-          return LOCAL.exec(moduleName);
-        })
-        .map(function (moduleName) {
-          return function (cb) {
-            fs.readFile(require.resolve(path.join(basepath, moduleName)), 'utf8', cb);
-          };
-        });
+module.exports = function pkgHash(modulePath) {
+	var readFiles = {};
 
-      async.parallel(tasks, function (err, results) {
-        if (err) {
-          done(err);
-        } else {
-          results.forEach(function (localSrc) {
-            src += localSrc;
-          });
-          done(null, crypto.createHash('md5').update(src).digest('hex'));
-        }
-      });
-    }
-  });
+	function getLocalRequires(srcPath) {
+		var hashes = [];
+
+		if (!readFiles[srcPath]) {
+			// process file
+			var src = fs.readFileSync(srcPath, 'utf8');
+			var hash = crypto.createHash('md5').update(src).digest('hex');
+			hashes.push({
+				path: srcPath,
+				hash: hash
+			});
+			readFiles[srcPath] = true;
+
+			detective(src)
+				.filter(function (r) {
+					return LOCAL.exec(r);
+				}).map(function (r) {
+					return resolve.sync(r, {
+						basedir: path.dirname(srcPath)
+					});
+				}).filter(function (r) {
+					return !readFiles[r];
+				}).forEach(function (r) {
+					hashes = hashes.concat(getLocalRequires(r));
+				});
+		}
+
+		return hashes;
+	}
+
+	var pkgPath = path.join(modulePath, 'package.json');
+	var pkgSrc = fs.readFileSync(pkgPath, 'utf8');
+	var pkg = JSON.parse(pkgSrc);
+	var entryFile = path.resolve(modulePath, pkg.main);
+	var requires = getLocalRequires(entryFile);
+
+	var hash = '';
+	for (var i = 0; i < requires.length; i++) {
+		hash = crypto.createHash('md5').update(hash + requires[i].hash).digest('hex');
+	}
+
+	return hash;
 };
--- test/test.js
+++ test/test.js
@@ -1,56 +1,37 @@
 'use strict';
 
 const path = require('path');
-const expect = require('expect');
+const assert = require('assert');
 const pkgHash = require('../pkg-hash');
 
+// TODO do proper testing this sucks
+
 describe('pkg-hash tests', () => {
-  it('should generate the same hashes for the same modules', done => {
-    pkgHash(path.join(__dirname, '..'), (err, hash1) => {
-      if (err) {
-        done(err);
-      } else {
-        pkgHash(path.join(__dirname, '..'), (err, hash2) => {
-          if (err) {
-            done(err);
-          } else {
-            expect(hash2).toEqual(hash1);
-            done();
-          }
-        });
-      }
-    });
-  });
+	it('should generate the same hashes for the same modules', () => {
+		const hash1 = pkgHash(path.join('test', '..'));
+		const hash2 = pkgHash(path.join('test', '..'));
+		assert.equal(hash2, hash1);
+	});
 
-  it('should generate different hashes for different modules', done => {
-    pkgHash(path.join(__dirname, 'fixtures', 'module0'), (err, hash1) => {
-      if (err) {
-        done(err);
-      } else {
-        pkgHash(path.join(__dirname, 'fixtures', 'module1'), (err, hash2) => {
-          if (err) {
-            done(err);
-          } else {
-            expect(hash2).toNotEqual(hash1);
-            done();
-          }
-        });
-      }
-    });
-  });
+	it('should generate different hashes for different modules', () => {
+		const hash1 = pkgHash(path.join('test', 'fixtures', 'module0'));
+		const hash2 = pkgHash(path.join('test', 'fixtures', 'module1'));
+		assert.notEqual(hash2, hash1);
+	});
 
-  it('should use all local files to generate the hash', done => {
-    pkgHash(path.join(__dirname, 'fixtures', 'module2'), done);
-  });
+	it('should use all local files to generate the hash', () => {
+		const hash = pkgHash(path.join('test', 'fixtures', 'module2'));
+		assert.ok(hash);
+	});
 
-  it('should work with weird file extensions', done => {
-    pkgHash(path.join(__dirname, 'fixtures', 'module3'), (err, hash) => {
-      if (err) {
-        done(err);
-      } else {
-        expect(hash).toExist();
-        done();
-      }
-    });
-  });
+	it('should work with weird file extensions', () => {
+		const hash = pkgHash(path.join('test', 'fixtures', 'module3'));
+		assert.ok(hash);
+	});
+
+	it('should generate different hashes for modules that produce the same output but using local & npm-insalled modules', () => {
+		const hash1 = pkgHash(path.join('test', 'fixtures', 'module4'));
+		const hash2 = pkgHash(path.join('test', 'fixtures', 'module5'));
+		assert.notEqual(hash1, hash2);
+	});
 });
